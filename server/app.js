a33d48684d74df2920478b91430a98d14dc96f38
hidhmat <hidhmatgroups@gmail.com>
2023-11-21 14:39:29 +0530

    check 26

--- server/app.js
+++ server/app.js
@@ -16,6 +16,13 @@ app.use(cors({
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 
 app.use('/public', express.static(__dirname + '/public'));
 app.options('*', cors());
