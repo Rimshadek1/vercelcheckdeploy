52daa16b099087cc579d04221d12a0ac7deb8a8c
hidhmat <hidhmatgroups@gmail.com>
2023-11-21 12:55:41 +0530

    check 19

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -18,7 +18,7 @@ function Login() {
         formData.append('number', number);
         formData.append('password', password);
 
-        axios.post('https://vercelcheckdeploy.vercel.app/login', formData)
+        axios.post('/login', formData)
             .then((res) => {
                 if (res.data.status === 'success') {
                     if (res.data.role === 'admin') {
