6887d23dc6bf4e2922d9608acfc4200a5efd42d1
hidhmat <hidhmatgroups@gmail.com>
2023-11-26 19:59:55 +0530

    check 57

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const createError = require('http-errors');
 
 const app = express();
 const corsOptions = {
-    origin: 'https://wwww.rimshad.tech',
+    origin: 'https://rimshad.tech',
     methods: ['GET', 'POST', 'DELETE'],
     credentials: true,
     optionsSuccessStatus: 204, // For preflight requests
