709094845e2fce8da6bb4dd38fcfaf9f8733ae23
hidhmat <hidhmatgroups@gmail.com>
2023-11-28 22:48:09 +0530

    check 98

--- server/Helpers/userHelper.js
+++ server/Helpers/userHelper.js
@@ -66,7 +66,9 @@ module.exports = {
         const profilePromise = db.get().collection(collection.imageCollection).find({}).toArray();
         const proofPromise = db.get().collection(collection.proofCollection).find({}).toArray();
 
-        console.log('3');
+
+        console.log('Profile:', profilePromise);
+        console.log('Proof:', proofPromise);
 
         try {
             // Wait for both promises to resolve
